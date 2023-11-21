385a9efc2081e97a2085426d8dc47fc6340c50e0
谢义普 <xieyipu@uebcorp.com>
2023-11-21 17:21:31 +0800

    swoft1.0 BD Query 查询修复 实体

---
+++ src/Query.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  group@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+namespace Swoft\Db;
+
+/**
+ * Query
+ */
+class Query
+{
+    /**
+     * @param string $tableName
+     * @param string $alias
+     *
+     * @return QueryBuilder
+     */
+    public static function table(string $tableName, string $alias = null): QueryBuilder
+    {
+        if(strpos($tableName, '`') !== false){
+            $tableName = str_replace('`','',$tableName);
+        }
+        $query = new QueryBuilder();
+        $query = $query->table($tableName, $alias);
+
+        return $query;
+    }
+}
